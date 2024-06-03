1b4fc2df841f878a98d90bf6eb65616a17f100d0
= <=>
2024-06-03 14:13:24 +0200

    Vercel test python 3

--- src/app/api/lookup/route.ts
+++ src/app/api/lookup/route.ts
@@ -1,15 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
-export async function POST(req:any) {
-
+export async function POST(req: any) {
   console.log("req:", req);
   const res = await req.json();
 
   const apiUrl = process.env.PYTHON_API_URL;
+  const data = "CARALHO Q NERVOSO";
+  // const response = await axios.post(`${apiUrl}/api/python/lookup`, res);
+  // const data = response.data;
 
-  const response = await axios.post(`${apiUrl}/api/python/lookup`, res);
-  const data = response.data;
-
-
-  return new Response(JSON.stringify(data))
+  return new Response(JSON.stringify(data));
 }
