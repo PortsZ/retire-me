072dbf37ae1b10210b0b9e11a213c27bf9315a1a
= <=>
2024-06-03 15:50:31 +0200

    Vercel test python 4

--- src/app/api/portfolio/rebalance/route.ts
+++ src/app/api/portfolio/rebalance/route.ts
@@ -4,7 +4,7 @@ export async function POST(req:any) {
   const res = await req.json();
   const apiUrl = process.env.PYTHON_API_URL;
 
-  const response = await axios.post(`${apiUrl}/api/python/rebalance`, res);
+  const response = await axios.post(`api/python/rebalance`, res);
   const data = response.data;
 
   return new Response(JSON.stringify(data))
